ead2a9e574ec0f44e202f84bf98966e5bd806df0
eoshackathon <40317002+eoshackathon@users.noreply.github.com>
2018-08-28 07:39:59 +0800

    Create StandardToken.sol

---
+++ solidity/StandardToken.sol
@@ -0,0 +1,120 @@
+pragma solidity ^0.4.24;
+
+import "https://github.com/eoshackathon/multi-stage-ico/solidity/ERC20Interface.sol";
+import "https://github.com/eoshackathon/multi-stage-ico/solidity/SafeMath.sol";
+import "https://github.com/eoshackathon/multi-stage-ico/solidity/Owned.sol";
+
+contract StandardToken is ERC20Interface, Owned{
+    using SafeMath for uint;
+    
+    string public symbol = "686";               //Symbol of Token
+    string public  name = "Everest  chain";                //Name of Token
+    uint256 public decimals = 18;              //decimals of Token
+    uint256 public _totalSupply = 2000000000 * 10**decimals;           //totalSupply of Token
+    address public owner;
+
+    mapping(address => uint) balances;  
+    mapping(address => mapping(address => uint)) allowed;
+    
+    constructor() public {
+        owner = msg.sender;
+        balances[owner] = _totalSupply;
+    }
+    
+    // ------------------------------------------------------------------------
+    // Total supply
+    // ------------------------------------------------------------------------
+    function totalSupply() public view returns (uint) {
+        return _totalSupply.sub(balances[address(0)]);
+    }
+
+    // ------------------------------------------------------------------------
+    // Get the token balance for account `tokenOwner`
+    // ------------------------------------------------------------------------
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    // ------------------------------------------------------------------------
+    // Change owner
+    // ------------------------------------------------------------------------
+    function changeOwner(address newOwner) public onlyOwner returns(bool) {
+        require(newOwner != address(0));
+        uint256 balanceOfOwner = balances[owner];
+        balances[owner] = 0;
+        owner = newOwner;
+        balances[owner] = balances[owner].add(balanceOfOwner);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Transfer the balance from token owner's account to `to` account
+    // - Owner's account must have sufficient balance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for `spender` to transferFrom(...) `tokens`
+    // from the token owner's account
+    //
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+    // recommends that there are no checks for the approval double-spend attack
+    // as this should be implemented in user interfaces 
+    // ------------------------------------------------------------------------
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Transfer `tokens` from the `from` account to the `to` account
+    // 
+    // The calling account must already have sufficient tokens approve(...)-d
+    // for spending from the `from` account and
+    // - From account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = balances[from].sub(tokens);
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for `spender` to transferFrom(...) `tokens`
+    // from the token owner's account. The `spender` contract function
+    // `receiveApproval(...)` is then executed
+    // ------------------------------------------------------------------------
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Owner can transfer out any accidentally sent ERC20 tokens
+    // ------------------------------------------------------------------------
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
+}
